92d0edb2a694eaa532dad15e7bdc73f4f72a98e2
Mansi <info@auspicioussoft.com>
2025-05-06 15:21:29 +0530

    fix: select specific fields for authorId in getAllCollectionsWithBooksService

--- src/services/collections/collections-service.ts
+++ src/services/collections/collections-service.ts
@@ -225,7 +225,7 @@ export const getAllCollectionsWithBooksService = async (payload: any, res: Respo
   }).skip(offset).limit(limit).select("-__v").populate({
     path: "booksId",
     populate: [
-      { path: "authorId" },
+      { path: "authorId", select: "name image" },
       {
         path: "categoryId",
         select: "name image",
