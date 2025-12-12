2a57042bf11b618b8d8a312705bcfa38f99381d9
Mansi <info@auspicioussoft.com>
2025-12-12 11:46:03 +0530

    fix: restore and update badge constants for accurate user level tracking

--- src/lib/constant.ts
+++ src/lib/constant.ts
@@ -9,25 +9,15 @@ export const httpStatusCode = {
     INTERNAL_SERVER_ERROR: 500,
 }
 
-// export const badges = [
-//       { count: 5, badge: "Saint", level: 1 },
-//       { count: 10, badge: "Hakim", level: 2 },
-//       { count: 20, badge: "Genius", level: 3 },
-//       { count: 25, badge: "Teacher" , level: 4 },
-//       { count: 40, badge: "Sufi", level: 5 },
-//       { count: 50, badge: "Expert", level: 6 },
-//       { count: 100, badge: "Commentator", level: 7 },
-//       { count: 125, badge: "Dervish", level: 8 },
-//       { count: 150, badge: "Murid", level: 8 },
-//     ];
 export const badges = [
-      { count: 1, badge: "Saint", level: 1 },
-      { count: 2, badge: "Hakim", level: 2 },
-      { count: 3, badge: "Genius", level: 3 },
-      { count: 4, badge: "Teacher" , level: 4 },
-      { count: 5, badge: "Sufi", level: 5 },
-      { count: 6, badge: "Expert", level: 6 },
-      { count: 7, badge: "Commentator", level: 7 },
-      { count: 8, badge: "Dervish", level: 8 },
-      { count: 9, badge: "Murid", level: 8 },
+      { count: 5, badge: "Saint", level: 1 },
+      { count: 10, badge: "Hakim", level: 2 },
+      { count: 20, badge: "Genius", level: 3 },
+      { count: 25, badge: "Teacher" , level: 4 },
+      { count: 40, badge: "Sufi", level: 5 },
+      { count: 50, badge: "Expert", level: 6 },
+      { count: 100, badge: "Commentator", level: 7 },
+      { count: 125, badge: "Dervish", level: 8 },
+      { count: 150, badge: "Murid", level: 8 },
     ];
+
